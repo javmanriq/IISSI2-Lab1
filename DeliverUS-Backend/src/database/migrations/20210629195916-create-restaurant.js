5348cfd80d3a59fbd6fb952b0709b9511e19fd8b
Javier Manrique <Javier.Manrique688@gmail.com>
2024-02-23 10:12:53 +0100

    hecho_en_clase

--- DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
+++ DeliverUS-Backend/src/database/migrations/20210629195916-create-restaurant.js
@@ -6,9 +6,78 @@ module.exports = {
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
-      }
+      },
       // TODO: Include the rest of the fields of the Restaurants table
-
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      adress: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      postalCode: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      restaurantCategoryId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'RestaurantCategories'
+          },
+          key: 'id'
+        }
+      },
+      shippingCosts: {
+        allowNull: false,
+        type: Sequelize.DOUBLE
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING,
+        unique: true
+      },
+      logo: {
+        type: Sequelize.STRING
+      },
+      phone: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: new Date()
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'Users'
+          },
+          key: 'id'
+        }
+      },
+      status: {
+        allowNull: false,
+        type: Sequelize.STRING
+      }
+      
     })
   },
   down: async (queryInterface, Sequelize) => {
